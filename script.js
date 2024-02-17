be5c9cc6c5ba5079869bfee375d8d979a385631c
Alperen <alperenbasali3@gmail.com>
2024-02-17 22:51:53 +0300

    break

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.getElementById('USD').addEventListener('click', function() {
     const arrow = document.querySelector('.arrow');
     arrow.classList.toggle('flipped');
+    
 });
+
+document.getElementById('USD').addEventListener('mousemove',function(){
+    document.getElementById('USD').style.color = "#6fcfb8"
+    document.getElementById('arrow').style.borderColor = "#6fcfb8 transparent transparent transparent"
+})
+
+document.getElementById('USD').addEventListener('mouseout',function(){
+    document.getElementById('USD').style.color = "rgb(189,189,189)"
+    document.getElementById('arrow').style.borderColor = "rgb(189,202,221) transparent transparent transparent"
+})
