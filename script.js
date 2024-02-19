af06fdf1e080f15b90f2f18cc876a008ea13e556
Alperen <alperenbasali3@gmail.com>
2024-02-19 23:17:06 +0300

    searchButton

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ document.getElementById('USD').addEventListener('mouseout',function(){
     document.getElementById('USD').style.color = "rgb(189,189,189)"
     document.getElementById('arrow').style.borderColor = "rgb(189,202,221) transparent transparent transparent"
 })
+
+document.querySelector(".searchButton").addEventListener("click", function(){
+    searchForm=document.getElementById("searchForm")
+    searchButton = document.querySelector(".searchButton")
+    biSearchSmall=document.querySelector(".biSearchSmall")
+     
+    if(searchForm.style.display == "none"){
+        searchForm.style.display="block"
+         searchButton.style.display="none"
+         biSearchSmall.style.display="none"
+        }
+})
+biSearchX=document.querySelector(".biSearchX")
+
+biSearchX.addEventListener("click",function(){
+    searchForm.style.display="none"
+    searchButton.style.display="block"
+    biSearchSmall.style.display="block"
+})
+
+
+
+  
