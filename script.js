2da80e1eb0b1a973abc6368865e499b571f21110
alperen <alperenbasali3@gmail.com>
2024-03-19 00:47:51 +0300

    mainpageDone

--- script.js
+++ script.js
@@ -437,3 +437,22 @@ $(document).ready(function () {
     }
 
 });
+
+
+
+// topbtn
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
